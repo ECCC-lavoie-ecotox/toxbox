be36121b0be15d9f285dcfd3f781fd9e74240a7f
Steve Vissault <s.vissault@yahoo.fr>
2024-08-05 11:41:23 -0400

    Refact database

--- R/fct_search.R
+++ R/fct_search.R
@@ -6,17 +6,15 @@ get_db <- function(con = NULL) {
     dplyr::tbl(
         con, dplyr::sql("
         SELECT * FROM lab_measurement
-            LEFT JOIN analyte USING (id_analyte)
-            LEFT JOIN lab_sample USING (id_lab_sample)
-            LEFT JOIN lab_field_sample USING (id_lab_sample)
-            LEFT JOIN field_sample USING (id_field_sample)
-            LEFT JOIN sites USING (id_site)
-            LEFT JOIN report USING (id_report)
-            LEFT JOIN project USING (id_project)
-            LEFT JOIN species USING (id_species)")
+            INNER JOIN analyte USING (id_analyte)
+            INNER JOIN lab_sample USING (id_lab_sample)
+            INNER JOIN lab_field_sample USING (id_lab_sample)
+            INNER JOIN field_sample USING (id_field_sample)
+            INNER JOIN sites USING (id_site)
+            INNER JOIN species USING (id_species)")
     )
 }
 
-#' Get full database cached
+#' Get full database cacheds
 #' @export
 get_db_cached <- memoise::memoise(get_db)
